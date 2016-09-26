30312213ef671233da060498df3f0552a18ade53
Can Geliş <geliscan@gmail.com>
2016-09-26 14:01:43 +0300

    --preserve is deprecated

--- backup.sh
+++ backup.sh
@@ -5,6 +5,6 @@ source $BACKUP_CWD/config
 service zimbra stop
 cd $ZIMBRA_PATH
 bash $BACKUP_CWD/hardlinks.sh scan . .hardlinks
-tar -f $BACKUP_FOLDER/zimbra.tgz --preserve -zc .
+tar -f $BACKUP_FOLDER/zimbra.tgz --preserve-permissions -zc .
 service zimbra start
 bash post_backup.sh
